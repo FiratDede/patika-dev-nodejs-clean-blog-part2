8fd90e955e15d8fb82c644c8c98ed4569b3d7efe
FIRAT DEDE <dede.firat.dede@gmail.com>
2022-01-26 20:53:22 +0300

    Clean Blog Project Part 3 Commit

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
-const express=require("express");
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app=express();
+const Post=require("./model/Post");
 
-app.listen(3000,()=>{
+const app = express();
+
+// connect db
+mongoose.connect("mongodb://127.0.0.1:27017/cleanblog-test-db", (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("Database connection succesfull");
+})
+// Start listening
+app.listen(3000, () => {
     console.log("Server is listening on 3000 port");
 })
 
 app.set('view engine', 'ejs');
 
-app.set("views","./views");
+app.set("views", "./views");
 
 
 
 app.use(express.static("public"));
 
+app.use(express.urlencoded());
+app.use(express.json());
 
-
-app.get("/",(req,res,next)=>{
-   res.render("index.ejs");
+// Home Page
+app.get("/", async (req, res, next) => {
+    let posts=await Post.find({});
+    res.render("index.ejs",{
+        posts
+    });
 })
-app.get("/about",(req,res,next)=>{
+
+// About Page
+app.get("/about", (req, res, next) => {
     res.render("./about");
 })
-app.get("/post",(req,res,next)=>{
+
+//Post Page
+app.get("/post", (req, res, next) => {
     res.render("./post");
 })
-app.get("/add_post",(req,res,next)=>{
+
+// Add Post Page
+app.get("/add_post", async (req, res, next)  => {
     res.render("./add_post");
 })
+
+// Handle New Post Data
+app.post("/posts",async (req,res,next)=>{
+    await Post.create(req.body);
+    res.redirect("/");
+
+})
+
